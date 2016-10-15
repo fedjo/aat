9b1ab8fa63b1825cb46fead4002f04ad2edb6b28
Yiorgos M <mari.giorgos@gmail.com>
2016-10-15 19:04:26 +0300

    generator to measure time

---
+++ thesis/utils/clock.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+import time
+import timeit
+
+class Clock:
+
+    @classmethod	
+    def time(self, func):
+	def timed(*args, **kwargs):
+	    t_start = time.time()
+	    result = func(*args, **kwargs)
+	    t_end = time.time()
+
+	    f = open(settings.BASE_DIR + '/timelapse.log', 'a')
+	    f.write('---Decorator time---\n')
+	    f.write('%r() %2.2f sec\n' % (func.__name__,  t_end-t_start))
+	    f.close()
+	    return result
+
+	return timed
+
+
+    @classmethod
+    def timeit(self, func):
+	def timed(*args, **kwargs):
+
+	    def wrapper(f, *arg, **kw):
+		def wrapped():
+		    return f(*args, **kwargs)
+		return wrapped
+
+	    wrapped = wrapper(func)
+	    f = open(settings.BASE_DIR + '/timelapse.log', 'a')
+	    f.write('---Decorator time-IT---\n')
+	    f.write('%r() %2.2f sec\n' % (func.__name__, timeit.timeit(wrapped)))
+	    f.close()
+	    return
+	
+	return timed
+
