5c588955e512f2d5f5f3f5fea6f0e291eeb36c57
Yiorgos M <mari.giorgos@gmail.com>
2016-10-28 17:23:59 +0300

    add Flat UI css frontend and relocate haar cascade files

--- thesis/forms.py
+++ thesis/forms.py
@@ -1,13 +1,22 @@
 from django import forms
 
 class VideoForm(forms.Form):
+
+    CHOICES = [('Yes', 'Yes'),
+               ('No', 'No')]
+
     title = "Please upload your video in zip format"
     video_dir = "" 
-    video = forms.FileField()
+    video = forms.FileField(widget=forms.FileInput(attrs={'class': 'form-control'}))
     recognizer = forms.MultipleChoiceField(choices=[('LBPH', 'Local Binary Patterns Histogram'),
 						    ('EF', 'Eighen Faces'),
-						    ('FF', 'Fisher Faces')])
+						    ('FF', 'Fisher Faces')],
+                            widget=forms.Select(attrs={'class': 'form-control select select-primary', 'data-toggle': 'select'}))
+    iszip = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'data-toggle': 'radio'}))
 
 class PostForm(forms.Form):
     title = "Please specify the directory containing your videos"
-    video_dir = forms.CharField()
+    video_dir = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    video_path = forms.FilePathField(path='/home/yiorgos/',  allow_folders=True)
+
+
