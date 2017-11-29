2165ff998b54bc3da4e6301a78aebbad308ebc29
Big Lebowski <gmar@image.ece.ntua.gr>
2017-11-29 21:09:49 +0200

    Admin interface for models

--- project/thesis/admin.py
+++ project/thesis/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from .models import Configuration, Cascade, RecognizerPreTrainedData
+
+
 # Register your models here.
+@admin.register(Configuration)
+class ConfigurationAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'cascade_name', 'recognizer_name',
+                    'objdetector_name', 'current']
+
+
+@admin.register(Cascade)
+class CascadeAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+
+
+@admin.register(RecognizerPreTrainedData)
+class RecognizerPreTrainedDataAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'recognizer']
