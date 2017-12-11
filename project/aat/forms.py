9ad95b0df94d894a8c6a22964d7ef5e49c094ac0
Big Lebowski <gmar@image.ece.ntua.gr>
2017-12-11 20:17:38 +0200

    Make forms more consistent

--- project/aat/forms.py
+++ project/aat/forms.py
@@ -3,12 +3,18 @@ from django import forms
 
 class DefaultDetectionForm(forms.Form):
     title = "Please specify the directory containing your videos"
-    video_dir = forms.CharField(widget=forms.TextInput(attrs={'class': \
-        'form-control'}), required=True)
-    recognizer = forms.CharField(widget=forms.HiddenInput(), required=True, \
-            initial=False)
-    objdetection = forms.CharField(widget=forms.HiddenInput(), required=True, \
-            initial=False)
+    video = forms.FileField(required=True, widget=forms.ClearableFileInput())
+    #video_dir = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),
+    #                            required=True)
+
+    detection = forms.CharField(widget=forms.HiddenInput(),
+                                    required=True, initial=False)
+    recognizer = forms.CharField(widget=forms.HiddenInput(),
+                                 required=True, initial=False)
+    objdetection = forms.CharField(widget=forms.HiddenInput(),
+                                   required=True, initial=False)
+    transcription = forms.CharField(widget=forms.HiddenInput(),
+                                    required=True, initial=False)
 
 
 class ComplexDetectionForm(forms.Form):
@@ -18,17 +24,31 @@ class ComplexDetectionForm(forms.Form):
 
     title = "Please upload your video in zip format"
     video = forms.FileField(required=True, widget=forms.ClearableFileInput())
-    subtitles = forms.BooleanField(required=False)
-    iszip = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect(attrs={'data-toggle': 'radio'}))
+    #video_dir = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),
+    #                            required=True)
+
+    detection = forms.CharField(widget=forms.HiddenInput(),
+                                    required=True, initial=False)
     recognizer = forms.ChoiceField(choices=[('LBPH', 'Local Binary Patterns Histogram'),
-						    ('EF', 'Eighen Faces'),
-						    ('FF', 'Fisher Faces'),
-						    ('KNN', 'LBPH using K-Nearest Neighbor'),
-                            ('No', 'Do not recognize faces')],
-                            widget=forms.Select(attrs={'class': 'form-control select select-primary', 'data-toggle': 'select'}))
+						                    ('EF', 'Eighen Faces'),
+						                    ('FF', 'Fisher Faces'),
+						                    #('KNN', 'LBPH using K-Nearest Neighbor'),
+                                            ('false', 'Do not recognize faces')],
+                                            widget=forms.Select(attrs={'class': 'form-control '
+                                                'select select-primary', 'data-toggle': 'select'}))
+    objdetection = forms.CharField(widget=forms.HiddenInput(),
+                                   required=True, initial=False)
+    transcription = forms.CharField(widget=forms.HiddenInput(),
+                                    required=True, initial=False)
+
+    iszip = forms.ChoiceField(choices=CHOICES,
+                              widget=forms.RadioSelect(attrs={'data-toggle': 'radio'}))
+
     scale = forms.FloatField(widget=forms.NumberInput(attrs={'class': 'form-control', 'step': '0.1'}))
     neighbors = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     min_x_dimension = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     min_y_dimension = forms.IntegerField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+
     bounding_boxes = forms.BooleanField(required=False)
-    facesdb = forms.FileField(required=False, widget=forms.ClearableFileInput())
+
+    # facesdb = forms.FileField(required=False, widget=forms.ClearableFileInput())
