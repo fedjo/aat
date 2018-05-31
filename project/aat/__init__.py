587a73ae04ca03f7fe235d35285bf0bb329a8020
Big Lebowski <gmar@image.ece.ntua.gr>
2018-05-31 16:47:04 +0300

    Add 2 more CNNs for object detection

--- project/aat/__init__.py
+++ project/aat/__init__.py
@@ -6,8 +6,13 @@ import tensorflow as tf
 CWD_PATH = os.path.join(os.getenv('FACEREC_APP_DIR', '..'), 'aat')
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-# MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_oid_2018_01_28'
+if True:
+    MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
+elif 1==2:
+    MODEL_NAME = 'faster_rcnn_inception_v2_coco_2018_01_28'
+else:
+    MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_oid_2018_01_28'
+
 PATH_TO_CKPT = os.path.join(CWD_PATH, 'object_detection', MODEL_NAME, 'frozen_inference_graph.pb')
 
 _detection_graph = tf.Graph()
